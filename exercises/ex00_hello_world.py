0ad2e085f30dd1a2cc7b3a1fe16ece9d9dd587ef
Krisha Avula <kravula@unc.edu>
2023-08-24 17:28:40 -0400

    My first program

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,5 @@
-"""My first program for COMPP110"""
+"""My first program for COMPP110."""
 
-print("Hello to this wonderful world")
+print("Hello, to this wonderful world.")
 
 _author_ = "730656379"
